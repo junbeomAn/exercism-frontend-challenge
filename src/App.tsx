449130f8fe938b01a8b2ec683a898d0b0ff0dbf1
junbeoman <wnsqja8744@gmail.com>
2022-04-11 23:29:06 +0900

    🎉 set up project with CRA

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TestimonialBox from './TestimonialBox/TestimonialBox.container';
+
+function App() {
+  return (
+    <div className='w-full flex justify-center'>
+      <TestimonialBox />
+    </div>
+  );
+}
+
+export default App;
