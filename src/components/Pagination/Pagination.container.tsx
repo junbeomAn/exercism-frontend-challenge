8944cc5d09a2b8c71284e6395a77888d84c43646
junbeoman <wnsqja8744@gmail.com>
2022-04-16 19:36:46 +0900

    refactor pagination component with more readable code

--- src/components/Pagination/Pagination.container.tsx
+++ src/components/Pagination/Pagination.container.tsx
@@ -8,13 +8,14 @@ import { IPageButton, IPagination } from './Pagination.entity';
 const Pagination = ({ totalPages, page, getNextPage }: IPagination) => {
   const idRef = useRef(-1);
 
-  const createProperPages = (start: number, end: number) => {
+  const createCenterPages = (start: number, end: number) => {
     const range: number[] = [];
 
     Array(end - start + 1)
       .fill(0)
       .forEach(() => {
-        if (start <= 0 || start > totalPages) {
+        const isOutOfRange = start <= 0 || start > totalPages;
+        if (isOutOfRange) {
           start += 1;
           return;
         }
@@ -24,40 +25,45 @@ const Pagination = ({ totalPages, page, getNextPage }: IPagination) => {
     return range;
   };
 
-  const getPageButtons = () => {
-    let start = page - 2;
-    let end = page + 2;
+  const addSidePages = (pages: number[]) => {
     const result: number[] = [];
-
-    const pages: number[] = createProperPages(start, end);
     const [firstPage, lastPage] = [pages[0], pages[pages.length - 1]];
 
-    if (firstPage - 1 >= 1) {
+    if (firstPage >= 2) {
       // first page
       result.push(1);
     }
-    if (firstPage - 1 > 1) {
+    if (firstPage > 2) {
       // front dots
       result.push(0);
     }
 
     result.push(...pages); // others
 
-    if (lastPage + 1 < totalPages) {
+    if (lastPage < totalPages - 1) {
       // rear dots
       result.push(0);
     }
-    if (lastPage + 1 <= totalPages) {
+    if (lastPage <= totalPages - 1) {
       // last page
       result.push(totalPages);
     }
+    return result;
+  };
+
+  const getPageButtons = (currentPage: number) => {
+    let start = currentPage - 2;
+    let end = currentPage + 2;
+
+    const pages: number[] = addSidePages(createCenterPages(start, end));
 
     return (
       <ul className='flex'>
-        {result.map((p, i) => {
+        {pages.map((p, i) => {
           idRef.current += 1;
+          const isDots = p === 0;
 
-          if (p === 0) {
+          if (isDots) {
             return (
               <li
                 className='w-[43px] h-page-btn text-center leading-[40px] mx-8px'
@@ -66,23 +72,24 @@ const Pagination = ({ totalPages, page, getNextPage }: IPagination) => {
                 ...
               </li>
             );
-          } else {
-            const isNextDots = result[i + 1] === 0;
-            return (
-              <li
-                key={idRef.current}
-                className={`${isNextDots ? '' : 'mr-12px'} last:mr-0`}
-              >
-                <PageButton
-                  type='number'
-                  selected={page === p}
-                  onClick={() => getNextPage(p)}
-                >
-                  {p}
-                </PageButton>
-              </li>
-            );
           }
+
+          const isNextDots = pages[i + 1] === 0;
+          const addMarginIfNextDots = isNextDots ? '' : 'mr-12px';
+          return (
+            <li
+              key={idRef.current}
+              className={`${addMarginIfNextDots} last:mr-0`}
+            >
+              <PageButton
+                type='number'
+                selected={page === p}
+                onClick={() => getNextPage(p)}
+              >
+                {p}
+              </PageButton>
+            </li>
+          );
         })}
       </ul>
     );
@@ -105,7 +112,7 @@ const Pagination = ({ totalPages, page, getNextPage }: IPagination) => {
           Previous
         </div>
       </PageButton>
-      {getPageButtons()}
+      {getPageButtons(page)}
       <PageButton
         type='arrow'
         disabled={page === totalPages}
@@ -135,27 +142,38 @@ function PageButton({
   selected = false,
   type = 'number',
 }: IPageButton) {
-  let customStyles = '';
+  const getPageBtnCustomStyles = (
+    type: string,
+    selected: boolean,
+    disabled: boolean
+  ) => {
+    let customStyles = '';
 
-  if (type === 'number') {
-    customStyles += `${
-      selected
-        ? `bg-light-highlight border-label-tertiary text-label-default `
-        : `bg-white border-light-border text-label-secondary `
-    }`;
-  } else if (type === 'arrow') {
-    customStyles += `${
-      disabled
-        ? `border-transparent bg-light-divider text-label-tertiary `
-        : `border-light-border text-label-secondary shadow-btn-active `
-    }`;
-  }
+    if (type === 'number') {
+      customStyles += `${
+        selected
+          ? `bg-light-highlight border-label-tertiary text-label-default `
+          : `bg-white border-light-border text-label-secondary `
+      }`;
+    } else if (type === 'arrow') {
+      customStyles += `${
+        disabled
+          ? `border-transparent bg-light-divider text-label-tertiary `
+          : `border-light-border text-label-secondary shadow-btn-active `
+      }`;
+    }
+    return customStyles;
+  };
 
   return (
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`h-page-btn leading-40px px-16px border rounded-inner ${customStyles} ${styles}`}
+      className={`h-page-btn leading-40px px-16px border rounded-inner ${getPageBtnCustomStyles(
+        type,
+        selected,
+        disabled
+      )} ${styles}`}
     >
       {children}
     </button>
