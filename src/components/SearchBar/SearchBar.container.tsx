c1a3b5a2456ce416e328ebc736fa28ca0aab6291
junbeoman <wnsqja8744@gmail.com>
2022-04-19 01:20:51 +0900

    improve SearchBar component more semantic

--- src/components/SearchBar/SearchBar.container.tsx
+++ src/components/SearchBar/SearchBar.container.tsx
@@ -13,15 +13,11 @@ const SearchBar = ({ setFilterStateValue }: ISearchBar) => {
     const { value } = e.target as HTMLInputElement;
     setFilterStateValue({ exercise: value });
   };
-  const onIconClick = () => {
-    inputRef.current?.focus();
-  };
-
   return (
-    <div
+    <label
       className={`w-search h-search ml-24px bg-dark-text-label-default rounded-inner px-21px py-11px flex items-center border border-transparent focus-within:shadow-filter-active focus-within:border-light-blue`}
     >
-      <SearchIcon onClick={onIconClick} />
+      <SearchIcon />
       <input
         ref={inputRef}
         className='flex items-center w-full h-full text-md leading-22px text-label-default placeholder-label-secondary bg-dark-text-label-default ml-16px focus:outline-none font-normal'
@@ -29,7 +25,7 @@ const SearchBar = ({ setFilterStateValue }: ISearchBar) => {
         placeholder={SEARCH_PLACEHOLDER}
         type='text'
       />
-    </div>
+    </label>
   );
 };
 
