baacad7504f126d69f19d2de496f81df004527c8
junbeoman <wnsqja8744@gmail.com>
2022-04-24 01:20:52 +0900

    fix throttle util function to work every specific time

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,14 +1,13 @@
 function throttle() {
   let timeout: NodeJS.Timeout | null = null;
 
-  return function <T>(cb: Function, time: number, ...args: T[]) {
+  return function <T>(cb: Function, time: number = 200, ...args: T[]) {
     if (timeout) {
       return;
     }
-
+    cb(...args);
     timeout = setTimeout(() => {
       timeout = null;
-      cb(...args);
     }, time);
   };
 }
