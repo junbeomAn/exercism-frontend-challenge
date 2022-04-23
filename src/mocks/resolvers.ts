f0f1c137e26a49999302169ad2a7745df59487b6
junbeoman <wnsqja8744@gmail.com>
2022-04-24 01:12:14 +0900

    modify msw resolver to test api requests

--- src/mocks/resolvers.ts
+++ src/mocks/resolvers.ts
@@ -1,6 +1,7 @@
 import { RestRequest, ResponseComposition, RestContext } from 'msw';
 
 import { testimonials, tracks } from './data';
+import { IMockTestimonialItem } from './entities';
 
 export const trackResolver = (
   req: RestRequest,
@@ -15,12 +16,59 @@ export const testimonialResolver = (
   res: ResponseComposition,
   ctx: RestContext
 ) => {
-  // const params = req.url.searchParams;
+  const params = req.url.searchParams;
 
-  // const exercise = params.get('exercise');
-  // const page = params.get('page');
-  // const track = params.get('track');
-  // const order = params.get('order');
+  const track = params.get('track');
+  const exercise = params.get('exercise');
+  const order = params.get('order');
+  const page = parseInt(params.get('page') || '1');
 
-  return res(ctx.json(testimonials));
+  const filtered = testimonials.testimonials.results
+    .filter(isTrack(track))
+    .filter(isExercise(exercise))
+    .sort(orderItems(order))
+    .slice(10 * (page - 1), 10 * page);
+
+  return res(
+    ctx.json({
+      testimonials: { ...testimonials.testimonials, results: filtered },
+    })
+  );
 };
+
+const isTrack = (trackName: string | null) => (item: IMockTestimonialItem) => {
+  if (!trackName) return true;
+  return trackName === item.track.slug;
+};
+
+const isExercise =
+  (exerciseName: string | null) => (item: IMockTestimonialItem) => {
+    if (!exerciseName) return true;
+    return item.exercise.title
+      .toLowerCase()
+      .includes(exerciseName.toLowerCase());
+  };
+
+const orderItems =
+  (orderBy: string | null) =>
+  (a: IMockTestimonialItem, b: IMockTestimonialItem) => {
+    if (orderBy === 'newest_first') {
+      if (a.created_at < b.created_at) {
+        return 1;
+      } else if (a.created_at > b.created_at) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else if (orderBy === 'oldest_first') {
+      if (a.created_at > b.created_at) {
+        return 1;
+      } else if (a.created_at < b.created_at) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else {
+      throw Error('Undefined order method is provided');
+    }
+  };
